adfcf8d1374d914f6742574f200fc66022ec2952
LillyTBui <73482787+LillyTBui@users.noreply.github.com>
2021-09-15 13:45:59 +0200

    blog details

--- js/blog-details.js
+++ js/blog-details.js
@@ -1,25 +1,22 @@
 const baseURL = "https://sunnyday.one/happy-tourist/wp-json/wp/v2/posts/";
 const id = JSON.parse(localStorage.getItem("post"));
 
-console.log(id);
-
 const newURL = baseURL + id + "?_embed";
 const postContainer = document.querySelector(".blog-specification");
 
 async function getPost(url) {
-    const response = await fetch(url);
-    const post = await response.json();
-    createPost(post);
-    console.log(post);
+  const response = await fetch(url);
+  const post = await response.json();
+  createPost(post);
 }
 
 function createPost(post) {
-    const date = new Date(post.date);
-    const day = date.getDay();
-    const month = date.getMonth();
-    const year = date.getFullYear();
+  const date = new Date(post.date);
+  const day = date.getDay();
+  const month = date.getMonth();
+  const year = date.getFullYear();
 
-    postContainer.innerHTML += `
+  postContainer.innerHTML += `
         <div class="post-details" data-id="${post.id}">
         <img src="${post._embedded["wp:featuredmedia"]["0"].media_details.sizes.full.source_url}" class="post-details__img" data-id="${post.id}">
             <h2 data-id="${post.id}">${post.title.rendered}</h2>
