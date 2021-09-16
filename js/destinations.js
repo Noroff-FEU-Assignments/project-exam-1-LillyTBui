358883e5df34539fbe61ce67472e4ddac89f22da
LillyTBui <73482787+LillyTBui@users.noreply.github.com>
2021-09-16 10:31:02 +0200

    destinations finished

--- js/destinations.js
+++ js/destinations.js
@@ -1,61 +1,51 @@
 const list = document.querySelectorAll(".list-a");
+window.onunload = function () {
+  sessionStorage.removeItem("count");
+};
 
+/* show destination list */
 for (const item of list) {
-    item.addEventListener(`click`, function () {
-        const child = item.children[0];
-        const currentCount = item.dataset.id;
-        const previousCount = JSON.parse(localStorage.getItem("count"));
-        let isSame = false;
-        let index;
+  item.addEventListener(`click`, function () {
+    const child = item.children[0];
+    const currentCount = item.dataset.id;
+    const previousCount = JSON.parse(localStorage.getItem("count"));
+    let isSame = false;
+    let index;
 
-        if (previousCount !== null) {
-            for (let i = 0; i <= previousCount.length-1; i++) {
-                console.log(currentCount, previousCount[i]);
-                    if(currentCount === previousCount[i]){ 
-                        isSame = true;
-                        index = previousCount.indexOf(currentCount);
-                    }
-            }
-            if(isSame === true){
-                child.style.display = "none";
-                previousCount.splice(index, 1);
-                localStorage.setItem("count", JSON.stringify(previousCount));
-            }
-            else{
-                child.style.display = "block";
-                previousCount.push(currentCount);
-                localStorage.setItem("count", JSON.stringify(previousCount));
-            }
+    if (previousCount !== null) {
+      for (let i = 0; i <= previousCount.length - 1; i++) {
+        if (currentCount === previousCount[i]) {
+          isSame = true;
+          index = previousCount.indexOf(currentCount);
         }
-        else {
-            child.style.display = "block";
-            const newList = [currentCount];
-            localStorage.setItem("count", JSON.stringify(newList));
-            }
-        }
-    )
-}
-
-
-/* child.style.display = "none";
-        const oldList = JSON.parse(localStorage.getItem("count"));
-        oldList.pop();
-        console.log("1");
-        localStorage.removeItem("count", JSON.stringify(oldList));
-        break; */
-
-        /*console.log("2");
+      }
+      if (isSame === true) {
+        child.style.display = "none";
+        previousCount.splice(index, 1);
+        localStorage.setItem("count", JSON.stringify(previousCount));
+      } else {
         child.style.display = "block";
         previousCount.push(currentCount);
-        localStorage.setItem("count", JSON.stringify(previousCount));*/
+        localStorage.setItem("count", JSON.stringify(previousCount));
+      }
+    } else {
+      child.style.display = "block";
+      const newList = [currentCount];
+      localStorage.setItem("count", JSON.stringify(newList));
+    }
+  });
+}
 
-       localStorage.clear();
+const favoriteContainer = document.querySelector(".favorite-destinations");
+const listContainer = document.querySelector(".destination-list__container");
 
-       /* const index = previousCount.indexOf(currentCount);
-                        const oldList = JSON.parse(localStorage.getItem("count"));
-                        oldList.splice(index, 1);
-                        localStorage.removeItem("count", JSON.stringify(oldList));
-                        const check = localStorage.setItem("count");
-                        console.log(check);
-                        i = previousCount.length;
-                        console.log(i);*/
+favoriteContainer.onclick = function (event) {
+  const id = event.target.dataset.id;
+  localStorage.setItem("post", JSON.stringify(id));
+};
+
+listContainer.onclick = function (event) {
+  const id = event.target.dataset.list;
+  console.log(id);
+  localStorage.setItem("post", JSON.stringify(id));
+};
